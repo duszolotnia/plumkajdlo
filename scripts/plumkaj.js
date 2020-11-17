c1bfaf5ed1ab18059c9578e099141dee6c550a20
duszolotnia <krzysztof.dolech.ext@nokia.com>
2020-11-17 11:12:12 +0100

    Change version to 1.0.1

--- scripts/plumkaj.js
+++ scripts/plumkaj.js
@@ -13,7 +13,7 @@ var SETTINGS = {
   highlight: "full",  
 }
 const SPLIT_CHAR = "|";
-const VERSION = "1.0.0";
+const VERSION = "1.0.1";
 const MENTION_CHAR = "@";
 const AVAIL_TITLES = [
   "Ktoś Cię oznaczył w komentarzu!",
