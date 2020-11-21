66939dcc03053cd47b60f8a880a7bf91c9575002
duszolotnia <krzysztof.dolech.ext@nokia.com>
2020-11-21 18:19:59 +0100

    Add bolding to common sections

---
+++ scripts/bold.js
@@ -0,0 +1,18 @@
+/* bold names of topics that are available for not logged in users */
+var common_section_url = [
+    "/tforum/ogolne",
+    "/tforum/wydarzenia",
+    "/tforum/sportowe",
+    "/tforum/osiagniecia",
+    "/tforum/wpa",
+    "/tforum/lekarze",
+    "/tforum/strzelnice",
+    "/tforum/sprzet",
+    "/tforum/sklepy",
+    "/tforum/oferty"
+];
+var a;
+common_section_url.forEach(sectionUrl => {
+    a = document.querySelector("a[href='"+sectionUrl+"']");
+    a.style = "font-weight: bold";
+});
